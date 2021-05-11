78cc65fb345eb44b266d1e2dd1c1517a8febb8f0
Daniil Chistyakov <4i.danila@gmail.com>
2021-05-11 17:14:45 +0300

    CORS errors fixed Security improved

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const app = express();
 const port = 5000;
 const film = require('./film');
 const helmet = require('helmet');
+const cors = require('cors');
+
 app.use(helmet());
 
+var corsOptions = {
+    origin: function (origin, callback) {
+        // Loading a list of allowed origins from the database
+        origins = ['http://localhost:3000', 'https://new.dyadka.gq', 'https://dyadka.gq', 'https://tv.dyadka.gq']
+        database.loadOrigins((error, origins) => {
+            callback(error, origins);
+        });
+    }
+}
+
+app.use(cors(corsOptions));
 app.get('/', (req, res) => {
     res.send('Отказано в доступе')
 })
