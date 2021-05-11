1a336a772a64430e9d608d931c1b906f00d2700d
Daniil Chistyakov <4i.danila@gmail.com>
2021-05-11 17:22:01 +0300

    CORS bug fixed

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ const cors = require('cors');
 
 app.use(helmet());
 
-var corsOptions = {
+app.use(cors({
     origin: function (origin, callback) {
-        // Loading a list of allowed origins from the database
-        origins = ['http://localhost:3000', 'https://new.dyadka.gq', 'https://dyadka.gq', 'https://tv.dyadka.gq']
-        database.loadOrigins((error, origins) => {
-            callback(error, origins);
-        });
-    }
-}
+        var whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://new.dyadka.gq', 'https://dyadka.gq', 'https://tv.dyadka.gq'];
 
-app.use(cors(corsOptions));
+        if (whitelist.indexOf(origin) === -1) {
+            var message = "The CORS policy for this origin doesn't allow access from the particular origin.";
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 app.get('/', (req, res) => {
     res.send('Отказано в доступе')
 })
