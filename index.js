02e681fdaf770263057182ef01b432a83f7a1bf8
Daniil Chistyakov <4i.danila@gmail.com>
2021-06-22 22:32:03 +0300

    Search added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const film = require('./film');
+const search = require('./search');
 const helmet = require('helmet');
 const cors = require('cors');
 
@@ -14,5 +15,6 @@ app.get('/', (req, res) => {
     res.send('Отказано в доступе')
 })
 app.use('/film', film);
+app.use('/search', search);
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
