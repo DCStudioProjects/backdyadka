208a79252bdbc2fd23206a505c3a1ed19e4cf11b
Daniil Chistyakov <4i.danila@gmail.com>
2021-06-24 13:33:07 +0300

    Details info added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const film = require('./film');
 const geturl = require('./geturl');
 const search = require('./search');
 const related = require('./related');
-const test = require('./test');
+const details = require('./details');
 const helmet = require('helmet');
 const cors = require('cors');
 
@@ -21,6 +21,6 @@ app.use('/film', film);
 app.use('/geturl', geturl);
 app.use('/search', search);
 app.use('/related', related);
-app.use('/test', test);
+app.use('/details', details);
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
