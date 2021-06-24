b2de88aff52d0a1c106a25473b96de8168c07b24
Daniil Chistyakov <4i.danila@gmail.com>
2021-06-24 11:24:41 +0300

    Test status bug fixed

--- test.js
+++ test.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/', async function (req, api) {
-    const test = (await axios.get(`https://rezkion.com/series/comedy/${req.query.id}-teoriya-bolshogo-vzryva-2007.html`)).data;
-    api.send({ test: test })
+    try {
+        const test = (await axios.get(`https://rezkion.com/series/comedy/${req.query.id}-teoriya-bolshogo-vzryva-2007.html`)).data;
+        api.send({ test: test })
+    } catch (e) {
+        api.send({ error: e })
+    }
 });
 
 module.exports = router;
