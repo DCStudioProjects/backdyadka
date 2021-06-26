3fb15c958a78089bd63e1157d72f91876847f9f3
Daniil Chistyakov <4i.danila@gmail.com>
2021-06-26 13:20:34 +0300

    Search engine changed

--- search.js
+++ search.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/', async function (req, api) {
-    const search = (await axios.get(`http://103.119.112.27/partner_api/suggests?query=${encodeURIComponent(req.query.q)}`,
-        {
-            headers: {
-                "X-FX-Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6ImZ4LTYwZDIyNGUwYzI0MTgifQ.eyJpc3MiOiJodHRwczpcL1wvZmlsbWl4Lm1lIiwiYXVkIjoiaHR0cHM6XC9cL2ZpbG1peC5tZSIsImp0aSI6ImZ4LTYwZDIyNGUwYzI0MTgiLCJpYXQiOjE2MjQzODQ3MzYsIm5iZiI6MTYyNDM3MzkzNiwiZXhwIjoxNjI2OTc2NzM2LCJwYXJ0bmVyX2lkIjoiMiIsImhhc2giOiI2MzM0ZDE4YzJkNmY5YTZiZTkyNTMwZDE5ZTIwOTI5NTA3ZjQ4OWIwIiwidXNlcl9pZCI6bnVsbCwiaXNfcHJvIjpmYWxzZSwiaXNfcHJvX3BsdXMiOmZhbHNlLCJzZXJ2ZXIiOiIifQ.xokKOK9NVe9Y8aqlxLpzDS23HRQFwLru3MAyz2eF1Ug"
-            }
-        })).data
+    try {
+        const search = (await axios.get(`https://filmix.beer/api/v2/suggestions?search_word=${encodeURIComponent(req.query.q)}`,
+            {
+                headers: {
+                    "x-requested-with": "XMLHttpRequest"
+                }
+            })).data
 
-    api.send({ search: search.items })
+        api.send({ search: search.posts })
+    } catch (err) {
+        api.send({ error: err })
+    }
 });
 
 module.exports = router;
