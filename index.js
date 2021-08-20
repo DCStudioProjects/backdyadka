8c43c9cfa7e78095de4f113e1def5a4aad369447
Daniel Chistyakov <4i.danila@gmail.com>
2021-08-20 18:46:35 +0300

    Bugs fixed

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
-const film = require('./film');
-const parsing = require('./parsing');
-const search = require('./search');
-const episodes = require('./episodes');
-const related = require('./related');
-const details = require('./details');
-const token = require('./token');
-const categories = require('./categories');
-const cors = require('cors');
+const film = require("./film");
+const parsing = require("./parsing");
+const search = require("./search");
+const episodes = require("./episodes");
+const related = require("./related");
+const details = require("./details");
+const token = require("./token");
+const categories = require("./categories");
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-    res.send('Отказано в доступе')
-})
-app.use('/parsing', parsing);
-app.use('/categories', categories);
-app.use('/search', search);
-app.use('/episodes', episodes);
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://new.dyadka.gq", "http://localhost:3000"],
+  })
+);
+app.get("/", (req, res) => {
+  res.send("Отказано в доступе");
+});
+app.use("/parsing", parsing);
+app.use("/categories", categories);
+app.use("/search", search);
+app.use("/episodes", episodes);
 /*app.use('/related', related);
 app.use('/details', details);
 app.use('/token', token);*/
 
-app.listen(port, () => console.log(`App is listening on port ${port}!`))
+app.listen(port, () => console.log(`App is listening on port ${port}!`));
