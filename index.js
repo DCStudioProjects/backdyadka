78f679a974c90f5fc8893dd98a963e1595c12ae9
Daniel Chistyakov <4i.danila@gmail.com>
2021-08-21 14:55:24 +0300

    Port fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const film = require("./film");
 const parsing = require("./parsing");
 const search = require("./search");
