11988407a04ace29d28b5a4a9ae0e13c1e21582f
Daniel Chistyakov <4i.danila@gmail.com>
2021-08-21 18:56:01 +0300

    Bugs fixed

--- parsing.js
+++ parsing.js
@@ -6,6 +6,29 @@ const querystring = require("querystring");
 
 router.get("/", async (req, api) => {
   try {
+    const headers = {
+      Accept: "application/json, text/javascript, */*; q=0.01",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+      Connection: "keep-alive",
+      "Content-Length": 102,
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      Cookie:
+        "dle_user_taken=1; dle_user_token=3590c8e8b68818e64a1c54288189afb9; _ym_uid=16230045491042370027; _ym_d=1623004549; _ga=GA1.2.1857658373.1625915394; _gid=GA1.2.1836983075.1629488381; _ym_isad=1; PHPSESSID=ap1c2e8ie42id2hs3d1vtrtf41; _ym_visorc=w",
+      Host: "rezkion.com",
+      Origin: "https://rezkion.com",
+      Referer:
+        "https://rezkion.com/series/comedy/1154-teoriya-bolshogo-vzryva-2007.html",
+      "sec-ch-ua":
+        '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+      "sec-ch-ua-mobile": "?0",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-origin",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+      "X-Requested-With": "XMLHttpRequest",
+    };
     const Translate = async () => {
       const rezkatranslate = (
         await axios.get(`http://f0561301.xsph.ru/?id=${req.query.id}`)
@@ -42,25 +65,31 @@ router.get("/", async (req, api) => {
         if (req.query.translation !== undefined) {
           const rezkaapi = await axios.post(
             "http://rezkance.com/ajax/get_cdn_series/",
-            querystring.stringify({
-              id: req.query.id,
-              translator_id: req.query.translation,
-              season: req.query.season,
-              episode: req.query.episode,
-              action: "get_episodes",
-            })
+            querystring.stringify(
+              {
+                id: req.query.id,
+                translator_id: req.query.translation,
+                season: req.query.season,
+                episode: req.query.episode,
+                action: "get_stream",
+              },
+              { headers }
+            )
           );
           return rezkaapi.data;
         } else {
           const rezkaapi = await axios.post(
             "http://rezkance.com/ajax/get_cdn_series/",
-            querystring.stringify({
-              id: req.query.id,
-              translator_id: (await Translate()).active.id,
-              season: req.query.season,
-              episode: req.query.episode,
-              action: "get_episodes",
-            })
+            querystring.stringify(
+              {
+                id: req.query.id,
+                translator_id: (await Translate()).active.id,
+                season: req.query.season,
+                episode: req.query.episode,
+                action: "get_stream",
+              },
+              { headers }
+            )
           );
           return rezkaapi.data;
         }
@@ -68,21 +97,27 @@ router.get("/", async (req, api) => {
         if (req.query.translation !== undefined) {
           const rezkaapi = await axios.post(
             "http://rezkance.com/ajax/get_cdn_series/",
-            querystring.stringify({
-              id: req.query.id,
-              translator_id: req.query.translation,
-              action: "get_movie",
-            })
+            querystring.stringify(
+              {
+                id: req.query.id,
+                translator_id: req.query.translation,
+                action: "get_movie",
+              },
+              { headers }
+            )
           );
           return rezkaapi.data;
         } else {
           const rezkaapi = await axios.post(
             "http://rezkance.com/ajax/get_cdn_series/",
-            querystring.stringify({
-              id: req.query.id,
-              translator_id: (await Translate()).active.id,
-              action: "get_movie",
-            })
+            querystring.stringify(
+              {
+                id: req.query.id,
+                translator_id: (await Translate()).active.id,
+                action: "get_movie",
+              },
+              { headers }
+            )
           );
           return rezkaapi.data;
         }
