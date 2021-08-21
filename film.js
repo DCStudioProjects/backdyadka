fcb433188a65e22544b9b8d0bd0a02630477919b
Daniel Chistyakov <4i.danila@gmail.com>
2021-08-21 15:47:53 +0300

    Film headers fixed

--- film.js
+++ film.js
@@ -5,8 +5,33 @@ const cheerio = require("cheerio");
 
 router.get("/", async function (req, api) {
   const { data } = await axios.get(
-    `https://rezkance.com/series/comedy/${req.query.id}-teoriya-bolshogo-vzryva-2007.html`
+    `https://rezkance.com/series/comedy/1154-teoriya-bolshogo-vzryva-2007.html`,
+    {
+      headers: {
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Cache-Control": "max-age=0",
+        Connection: "keep-alive",
+        Cookie:
+          "dle_user_taken=1; dle_user_token=3590c8e8b68818e64a1c54288189afb9; _ym_uid=16230045491042370027; _ym_d=1623004549; _ga=GA1.2.1857658373.1625915394; _gid=GA1.2.1836983075.1629488381; _ym_isad=1; PHPSESSID=ap1c2e8ie42id2hs3d1vtrtf41; _ym_visorc=b",
+        Host: "rezkance.com",
+        Referer: "https://rezkance.com/",
+        "sec-ch-ua":
+          '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+        "sec-ch-ua-mobile": "?0",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": 1,
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+      },
+    }
   );
+
   const selector = cheerio.load(data);
   const title = selector("h1").text();
   const origtitle = selector(".b-post__origtitle").text();
