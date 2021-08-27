09b3a8ac080fa9a2f20cbc6168f5ff11798fae9f
Daniel Chistyakov <4i.danila@gmail.com>
2021-08-27 16:59:55 +0300

    Critical bug fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 1000;
+const port = process.env.PORT || 5000;
 const categories = require("./categories");
 //const details = require("./details"); NEEDS WORK!
 const film = require("./film");
