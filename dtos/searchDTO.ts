094f0f7242daa5a014da226c819eeb261f334bfc
daniilchistyakov <d.chistyakov@workoutme.app>
2022-01-20 22:37:12 +0300

    Search endpoint optimized

--- dtos/searchDTO.ts
+++ dtos/searchDTO.ts
@@ -1,15 +1,14 @@
 export const searchDTO = (item: any) => {
   const {
-    id,
-    title_rus: title,
-    kinopoisk_id: kpId,
+    nameRu: title,
+    kinopoiskId: kpId,
     poster: defaultPoster,
   } = item;
-  let poster = `https://cdn.statically.io/img/kinopoiskapiunofficial.tech/f=auto,q=60,h=300/images/posters/kp/${kpId}.jpg`;
+  let poster = `https://cdn.statically.io/img/kinopoiskapiunofficial.tech/f=auto,q=60,h=300/images/posters/kp_small/${kpId}.jpg`;
 
   if (!kpId || !defaultPoster) {
     poster =
-      "https://cdn.statically.io/img/i.pinimg.com/f=auto,q=60,h=300/originals/42/2f/83/422f83814d816e55ce6a633f35ec63a4.jpg";
+      'https://cdn.statically.io/img/i.pinimg.com/f=auto,q=60,h=300/originals/42/2f/83/422f83814d816e55ce6a633f35ec63a4.jpg';
   }
-  return { id, kpId, poster, title };
+  return {kpId, poster, title};
 };
--- routes/search.ts
+++ routes/search.ts
@@ -1,31 +1,22 @@
-import express, { Request, Response } from "express";
-import { $mediaData } from "../api/ApiVars";
-import { errorHandler } from "../errorHandler";
-import { searchDTO } from "../dtos/searchDTO";
+import express, {Request, Response} from 'express';
+import {$kpdata} from '../api/ApiVars';
+import {errorHandler} from '../errorHandler';
+import {searchDTO} from '../dtos/searchDTO';
 
 const router = express.Router();
 
-router.post("/search", async (req: Request, api: Response) => {
-  const { query } = req.body;
+router.post('/search', async (req: Request, api: Response) => {
   try {
-    const title = query ? `&title=${encodeURIComponent(query)}` : "";
-    const { data: rawData } = await $mediaData.get(
-      `/search?account_key=${process.env.MEDIA_KEY}${title}`
+    const {query} = req.body;
+    const {data: rawData} = await $kpdata.get(
+      `/v2.2/films?order=NUM_VOTE&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000&keyword=${encodeURIComponent(query)}&page=1`
     );
-    const data = rawData.result
-      ?.filter((item) => {
-        if (item.kinopoisk_id) {
-          return item;
-        }
-      })
-      .map((item) => {
-        const result = searchDTO(item);
-        return result;
-      });
+    const data = rawData.items.map((item) => {
+      const result = searchDTO(item);
+      return result;
+    });
     api.send({
-      data: data || [],
-      defaultPoster:
-        "https://cdn.statically.io/img/i.pinimg.com/f=auto,q=60,h=300/originals/42/2f/83/422f83814d816e55ce6a633f35ec63a4.jpg",
+      data,
     });
   } catch (e) {
     errorHandler(e, api);
