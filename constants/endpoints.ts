1489fac30e37d92f37990c6ab8b9f69de509e28a
Daniel Chistyakov <4i.danila@gmail.com>
2022-03-08 14:46:13 +0300

    Thrillers added

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -3,6 +3,7 @@ export const categories = {
   films: '/top?type=TOP_100_POPULAR_FILMS',
   series: '?order=RATING&type=TV_SHOW&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000',
   comedy: '?genres=13&order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000',
+  thrillers: '?genres=11&order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000',
   kids: '?genres=33&order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000',
   tv: '?genres=30&order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000',
   sport: '?genres=21&order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000',
